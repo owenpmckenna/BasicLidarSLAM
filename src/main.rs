4afa4be78bf8bbb3808d200e0070681b6c2e1fe0
owen <owenpmckenna@outlook.com>
2025-09-10 11:25:59 -0400

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+extern crate serialport;
+fn main() {
+    println!("Hello, world!");
+    use rplidar_drv::RplidarDevice;
+
+    let mut serial_port = serialport::new("/dev/usbtty0", 1).open().unwrap();
+    let mut rplidar = RplidarDevice::with_stream(serial_port);
+
+    let device_info = rplidar.get_device_info().unwrap();
+    rplidar.start_scan().unwrap();
+
+    while true {
+        let scan_point = rplidar.grab_scan_point().unwrap();
+
+        println!("dist: {}", scan_point.dist_mm_q2);
+        println!("angle: {}", scan_point.angle_z_q14);
+    }
+}
