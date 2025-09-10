2c9eadc3c33d92f8d99c8bb7b630fb66f0e982da
owen <owenpmckenna@outlook.com>
2025-09-10 12:06:09 -0400

    Increase baud rate, attempt to fix timeout

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 extern crate serialport;
+
+use serial2::SerialPort;
+
 fn main() {
     println!("Hello, world!");
     use rplidar_drv::RplidarDevice;
-
-    let mut serial_port = serialport::new("/dev/ttyUSB0", 1).open().unwrap();
+    //let serial_port = SerialPort::open("/dev/ttyUSB0".to_owned(), 115200).unwrap();
+    let mut serial_port = serialport::new("/dev/ttyUSB0", 115200).open().unwrap();
     let mut rplidar = RplidarDevice::with_stream(serial_port);
 
     let device_info = rplidar.get_device_info().unwrap();
