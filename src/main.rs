d06baaf9230d1cba533eecff540c26211e60d1fb
owen <owenpmckenna@outlook.com>
2025-09-12 09:45:30 -0400

    Changed baud rate
    Python code doesn't explicitly start scan, will try that next. Baud rate from python library

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     println!("Hello, world!");
     use rplidar_drv::RplidarDevice;
     //let serial_port = SerialPort::open("/dev/ttyUSB0".to_owned(), 115200).unwrap();
-    let serial_port = serialport::new("/dev/ttyUSB0", 115200).open().unwrap();
+    let serial_port = serialport::new("/dev/ttyUSB0", 256000).open().unwrap();
     let mut rplidar = RplidarDevice::with_stream(serial_port);
 
     let device_info = rplidar.get_device_info().unwrap();
