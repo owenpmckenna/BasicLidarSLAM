8eec64252a6eb9e81be0e89cb8f73f8f0cdf0a47
owen <owenpmckenna@outlook.com>
2025-09-15 13:54:47 -0400

    Print in csv

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use serialport::{DataBits, FlowControl, Parity, SerialPortSettings, StopBits};
 //use serial2::SerialPort;
 
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
     use rplidar_drv::RplidarDevice;
     //let serial_port = SerialPort::open("/dev/ttyUSB0".to_owned(), 115200).unwrap();\
     let s = SerialPortSettings {
@@ -26,12 +26,12 @@ fn main() {
     let mut rplidar = RplidarDevice::with_stream(serial_port);
 
     let device_info = rplidar.get_device_info().unwrap();
-    println!("device info: {:?}", device_info);
+    //println!("device info: {:?}", device_info);
     rplidar.set_motor_pwm(500).expect("Motor start failed somehow");
-    println!("start motor done");
+    //println!("start motor done");
     //sleep(Duration::from_secs(5));
     let scan_type = rplidar.start_scan_with_options(&ScanOptions::force_scan()).unwrap();
-    println!("scan type: {:?}", scan_type);
+    //println!("scan type: {:?}", scan_type);
     //let health = rplidar.get_device_health().unwrap();
     //println!("health: {:?}", health);
 
@@ -41,14 +41,13 @@ fn main() {
     let mut total: f32 = 0.0;
     loop {
         for scan_point in rplidar.grab_scan_with_timeout(Duration::from_secs(15)).unwrap() {
-            //println!("dist: {}", scan_point.distance());
-            //println!("angle: {}", scan_point.angle());
+            print!("{},", scan_point.distance());
+            println!("{}", scan_point.angle());
             //println!("x: {}", x);
-            total += scan_point.distance();
             x += 1;
         }
-        println!("total: {}", total/(x as f32));
-        println!("y: {}", y);
+        //println!("total: {}", total/(x as f32));
+        //println!("y: {}", y);
         y += 1;
         //x = 0;
     }
