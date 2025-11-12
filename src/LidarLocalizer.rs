86bffef94c95801354e7284e9b1b20d4d7fc2e98
owen <owenpmckenna@outlook.com>
2025-11-12 16:23:58 -0500

    ok i forget to fix the index

--- src/LidarLocalizer.rs
+++ src/LidarLocalizer.rs
@@ -128,7 +128,7 @@ impl InstantLidarLocalizer {
             .collect();
         let mut i = 2usize;
         let mut lines = Vec::new();
-        while i < altered_points.len()-2 {
+        while i < altered_points.len()-5 {
             let line = InstantLine::is_line(altered_points[i..i+InstantLine::INIT_LINE_POINTS].try_into().unwrap());//test if consecutive points are in a line
             match line {
                 Some(mut it) => {
